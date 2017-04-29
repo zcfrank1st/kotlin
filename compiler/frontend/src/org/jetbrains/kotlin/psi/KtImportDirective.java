351491e53c5e90ff02e9821533cea0ae3ebb47de
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2017-04-29 19:33:56 +0300

    Refactoring: return Import interface from KtImportDirective

--- compiler/frontend/src/org/jetbrains/kotlin/psi/KtImportDirective.java
+++ compiler/frontend/src/org/jetbrains/kotlin/psi/KtImportDirective.java
@@ -83,7 +83,7 @@ public class KtImportDirective extends KtElementImplStub<KotlinImportDirectiveSt
 
     @Nullable
     @IfNotParsed
-    public ImportPath getImportPath() {
+    public Import getImportPath() {
         FqName importFqn = getImportedFqName();
         if (importFqn == null) {
             return null;
