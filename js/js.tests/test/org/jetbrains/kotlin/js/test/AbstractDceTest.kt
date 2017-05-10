a6d50be6ead9794441348e704cd1a0180a85a54e
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2017-05-10 15:23:29 +0300

    ~fixup 862ae2a2470f2616b4a470834fd6c6838083e53f

--- js/js.tests/test/org/jetbrains/kotlin/js/test/AbstractDceTest.kt
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/AbstractDceTest.kt
@@ -16,17 +16,15 @@
 
 package org.jetbrains.kotlin.js.test
 
-import com.intellij.openapi.util.io.FileUtil
 import junit.framework.TestCase
 import org.jetbrains.kotlin.js.dce.DeadCodeElimination
 import org.jetbrains.kotlin.js.dce.InputFile
 import java.io.File
-import java.util.regex.Pattern
 
 abstract class AbstractDceTest : TestCase() {
     fun doTest(filePath: String) {
         val file = File(filePath)
-        val fileContents = FileUtil.loadFile(file)
+        val fileContents = file.readText()
         val inputFile = InputFile(filePath, File(pathToOutputDir, file.relativeTo(File(pathToTestDir)).path).path, "main")
         val dceResult = DeadCodeElimination.run(setOf(inputFile), extractDeclarations(REQUEST_REACHABLE_PATTERN, fileContents)) {}
         val reachableNodeStrings = dceResult.reachableNodes.map { it.toString().removePrefix("<unknown>.") }.toSet()
@@ -39,19 +37,13 @@ abstract class AbstractDceTest : TestCase() {
         }
     }
 
-    private fun extractDeclarations(pattern: Pattern, fileContents: String): Set<String> {
-        val matcher = pattern.matcher(fileContents)
-        val result = mutableSetOf<String>()
-        while (matcher.find()) {
-            result += matcher.group(1)
-        }
-        return result
-    }
+    private fun extractDeclarations(regex: Regex, fileContents: String): Set<String> =
+            regex.findAll(fileContents).map { it.groupValues[1] }.toSet()
 
     companion object {
-        private val ASSERT_REACHABLE_PATTERN = Regex("^ *// *ASSERT_REACHABLE: (.+) *$", RegexOption.MULTILINE).toPattern()
-        private val ASSERT_UNREACHABLE_PATTERN = Regex("^ *// *ASSERT_UNREACHABLE: (.+) *$", RegexOption.MULTILINE).toPattern()
-        private val REQUEST_REACHABLE_PATTERN = Regex("^ *// *REQUEST_REACHABLE: (.+) *$", RegexOption.MULTILINE).toPattern()
+        private val ASSERT_REACHABLE_PATTERN = Regex("^ *// *ASSERT_REACHABLE: (.+) *$", RegexOption.MULTILINE)
+        private val ASSERT_UNREACHABLE_PATTERN = Regex("^ *// *ASSERT_UNREACHABLE: (.+) *$", RegexOption.MULTILINE)
+        private val REQUEST_REACHABLE_PATTERN = Regex("^ *// *REQUEST_REACHABLE: (.+) *$", RegexOption.MULTILINE)
 
         private val pathToTestDir = "js/js.translator/testData/dce"
         private val pathToOutputDir = "js/js.translator/testData/out/dce"
