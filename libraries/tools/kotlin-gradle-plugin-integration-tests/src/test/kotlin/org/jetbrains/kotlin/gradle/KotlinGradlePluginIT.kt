79776215ce77a8869efa477f10534a5cca03fc48
Sergey Igushkin <hotkeytlt@gmail.com>
2017-05-19 14:16:47 +0300

    Pass freeCompilerArgs unchanged to the compiler, without parsing them with the compiler loaded along the Gradle plugin.

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/KotlinGradlePluginIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/KotlinGradlePluginIT.kt
@@ -401,9 +401,9 @@ class KotlinGradleIT: BaseGradleIT() {
         val project = Project("kotlinProject", GRADLE_VERSION)
         project.setupWorkingDir()
 
+        val customModuleName = "custom_module_name"
+
         File(project.projectDir, "build.gradle").modify {
-            // lazy eval is important
-            val customModuleName = "\${project.name}"
             it + """
             compileKotlin {
                 kotlinOptions.freeCompilerArgs = [ "-module-name", "$customModuleName" ]
@@ -412,6 +412,7 @@ class KotlinGradleIT: BaseGradleIT() {
 
         project.build("build") {
             assertSuccessful()
+            assertFileExists("build/classes/main/META-INF/$customModuleName.kotlin_module")
         }
     }
 
